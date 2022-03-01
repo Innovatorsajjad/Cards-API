ab5206a2e06841e5dfa141a6ff583e9230ed40d8
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-01 06:23:24 +0600

    added api link

--- javascript/card.js
+++ javascript/card.js
@@ -1,1 +1,5 @@
-console.log('hlw javascript')
+// fetch Data.............
+const url = "https://deckofcardsapi.com/api/deck/new/draw/?count=52"
+fetch (url)
+.then(Response => Response.json())
+.then(data => console.log(data))
