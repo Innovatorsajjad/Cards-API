6dad29945df094d1aa221f35808d32aacc47cb9f
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-01 09:15:39 +0600

    error added

--- javascript/card.js
+++ javascript/card.js
@@ -1,5 +1,23 @@
+//Button EventHandeler setup.
+//get Input value.
+//Error Handaling..for stritng value.
+
+
 // fetch Data.............
-const url = "https://deckofcardsapi.com/api/deck/new/draw/?count=52"
-fetch (url)
-.then(Response => Response.json())
-.then(data => console.log(data))
+const SearchButton =()=>{
+    const Inplut = document.getElementById("input_value");
+    const Error = document.getElementById("Error")
+    const inputValue = Inplut.value;
+    console.log(inputValue);
+
+    if(isNaN(inputValue)){
+        Error.innerText="Please inter a Number"
+        
+    }
+
+//     const url = "https://deckofcardsapi.com/api/deck/new/draw/?count=52"
+// fetch (url)
+// .then(Response => Response.json())
+// .then(data => console.log(data))
+}
+
